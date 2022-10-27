133e9a7c9319b94abf8811c66db809afaa7b8a4b
Tom Madelin <tom.madelin@gmail.com>
2022-10-27 12:04:55 +0100

    download script now cleans data before writing to master

--- s3_test.py
+++ s3_test.py
@@ -1,4 +1,6 @@
-import requests, json, csv
+import requests
+import json
+import csv
 
 urls = []
 
@@ -14,12 +16,14 @@ for url in urls:
     print(url)
 
 # open file
-f = open('./all_trips_2.csv', 'w')
+f = open('./cleaned_data.csv', 'w')
 # create the csv writer
 writer = csv.writer(f)
 
 # download csv data from urls and write to master csv file
+first_row_count = 0
 for url in urls:
+    individual_row_count = 0
     with requests.Session() as s:
         print(f'Downloading: {url}')
         download = s.get(url)
@@ -32,7 +36,38 @@ for url in urls:
 
         print("Writing to file")
         for row in my_list:
-            writer.writerow(row)
+            # leave in one header row
+            if first_row_count == 0:
+                writer.writerow(row)
+
+            # ignore all other header rows
+            if individual_row_count != 0:
+
+                # ignore any rows with missing data
+                if (row[0] == '' or row[1] == '' or row[2] == '' or row[3] == '' or row[4] == '' or row[5] == '' or row[6] == '' or row[7] == '' or row[8] == ''):
+                    print(f"Empty field! \n {row}")
+
+                # TODO: ignore rows that have the wrong data type in the corresponding column
+                # check for start date being '0' or '1' here (row[6])
+                # HACK: The only rows with startdate being '0' or '1' have a length of 10
+                elif (len(row) == 10):
+                    print("Length of row = 10, ignoring row[6]")
+                    writer.writerow(
+                        [row[0], row[1], row[2], row[3], row[4], row[5], row[7], row[8], row[9]])
+
+                # write only the rows that correspond to the header rows (ignore index's above 9)
+                # HACK: some rows have random empty fields at the end, but the rest of the data is fine... so we only write the useful data
+                elif len(row) > 9:
+                    print("Row contains trailing comma's, ignoring")
+                    writer.writerow(
+                        [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]])
+
+                # if we get here the row is formatted correctly
+                else:
+                    writer.writerow(row)
+
+            individual_row_count += 1
+            first_row_count += 1
 
         print("Done!")
 
